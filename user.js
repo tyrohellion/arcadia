4ee9d0da310eacd498a07dcf58c563183a6b57fd
tyrohellion <51808054+tyrohellion@users.noreply.github.com>
2023-11-13 23:23:14 -0600

    Update user.js to reflect newest fastfox version

--- user.js
+++ user.js
@@ -5,10 +5,21 @@ user_pref("nglayout.initialpaint.delay", 0);
 user_pref("nglayout.initialpaint.delay_in_oopif", 0);
 user_pref("content.notify.interval", 100000);
 user_pref("browser.startup.preXulSkeletonUI", false);
+user_pref("gfx.canvas.accelerated.cache-items", 4096); // default=2048; alt=8192
+user_pref("gfx.canvas.accelerated.cache-size", 512); // default=256; alt=1024
+user_pref("gfx.content.skia-font-cache-size", 20);
+user_pref("browser.cache.jsbc_compression_level", 3);
+user_pref("media.memory_cache_max_size", 65536);
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.cache_resume_threshold", 3600);
+user_pref("image.mem.decode_bytes_at_a_time", 32768);
+
 
 /** EXPERIMENTAL ***/
 user_pref("layout.css.grid-template-masonry-value.enabled", true);
 user_pref("dom.enable_web_task_scheduling", true);
+user_pref("layout.css.has-selector.enabled", true);
+user_pref("dom.security.sanitizer.enabled", true);
 
 /** GFX ***/
 user_pref("gfx.webrender.all", true);
@@ -38,6 +49,14 @@ user_pref("network.buffer.cache.count", 128);
 user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
 user_pref("network.ssl_tokens_cache_capacity", 32768);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.dns.max_high_priority_threads", 8);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.prefetch-next", false);
+user_pref("network.predictor.enabled", false);
 
 /** CUSTOM CSS ***/
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
