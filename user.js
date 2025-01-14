e1b2538a2ccbc065dbc2e9729ae2958817aa6bdb
tyrohellion <51808054+tyrohellion@users.noreply.github.com>
2025-01-14 17:35:46 -0500

    VP9 tweaks, update fastfox (betterfox), and disable smooth scrolling
    -added value that makes VP9 (YouTube) more performant when playing videos

--- user.js
+++ user.js
@@ -1,69 +1,39 @@
-/****************************************************************************
- * SECTION: FASTFOX                                                         *
-****************************************************************************/
-user_pref("nglayout.initialpaint.delay", 0);
-user_pref("nglayout.initialpaint.delay_in_oopif", 0);
+/** GENERAL ***/
 user_pref("content.notify.interval", 100000);
-user_pref("browser.startup.preXulSkeletonUI", false);
-user_pref("gfx.canvas.accelerated.cache-items", 4096); // default=2048; alt=8192
-user_pref("gfx.canvas.accelerated.cache-size", 512); // default=256; alt=1024
+
+/** GFX ***/
+user_pref("gfx.canvas.accelerated.cache-items", 4096);
+user_pref("gfx.canvas.accelerated.cache-size", 512);
 user_pref("gfx.content.skia-font-cache-size", 20);
-user_pref("browser.cache.jsbc_compression_level", 3);
+
+/** DISK CACHE ***/
+user_pref("browser.cache.disk.enable", true);
+
+/** MEDIA CACHE ***/
 user_pref("media.memory_cache_max_size", 65536);
 user_pref("media.cache_readahead_limit", 7200);
 user_pref("media.cache_resume_threshold", 3600);
+
+/** IMAGE CACHE ***/
 user_pref("image.mem.decode_bytes_at_a_time", 32768);
 
-
-/** EXPERIMENTAL ***/
-user_pref("layout.css.grid-template-masonry-value.enabled", true);
-user_pref("dom.enable_web_task_scheduling", true);
-user_pref("layout.css.has-selector.enabled", true);
-user_pref("dom.security.sanitizer.enabled", true);
-
-/** GFX ***/
-user_pref("gfx.webrender.all", true);
-user_pref("gfx.webrender.precache-shaders", true);
-user_pref("gfx.webrender.compositor", true);
-user_pref("layers.gpu-process.enabled", true);
-user_pref("layers.acceleration.force-enabled", true);
-user_pref("media.hardware-video-decoding.enabled", true);
-user_pref("gfx.canvas.accelerated", true);
-user_pref("gfx.canvas.accelerated.cache-items", 32768);
-user_pref("gfx.canvas.accelerated.cache-size", 4096);
-user_pref("gfx.content.skia-font-cache-size", 80);
-user_pref("image.cache.size", 10485760);
-user_pref("image.mem.decode_bytes_at_a_time", 131072);
-user_pref("image.mem.shared.unmap.min_expiration_ms", 120000);
-user_pref("media.memory_cache_max_size", 1048576);
-user_pref("media.memory_caches_combined_limit_kb", 2560000);
-user_pref("media.cache_readahead_limit", 9000);
-user_pref("media.cache_resume_threshold", 6000);
-
-/** BROWSER CACHE ***/
-user_pref("browser.cache.memory.max_entry_size", 153600);
-
 /** NETWORK ***/
-user_pref("network.buffer.cache.size", 262144);
-user_pref("network.buffer.cache.count", 128);
 user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
-user_pref("network.ssl_tokens_cache_capacity", 32768);
 user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
 user_pref("network.http.pacing.requests.enabled", false);
 user_pref("network.dnsCacheExpiration", 3600);
-user_pref("network.dns.max_high_priority_threads", 8);
 user_pref("network.ssl_tokens_cache_capacity", 10240);
-user_pref("network.dns.disablePrefetch", true);
-user_pref("network.prefetch-next", false);
-user_pref("network.predictor.enabled", false);
+
+/** YOUTUBE (better VP9 performance for my system) ***/
+user_pref("media.wmf.dxva.d3d11.enabled", false);
 
 /** CUSTOM CSS ***/
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
 
-/** EDGE LIKE SMOOTH SCROLL ***/
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
+/** DISABLE SMOOTH SCROLLING (personal preference) ***/
+user_pref("general.smoothScroll", false);
 
 /** REMOVE FIREFOX VIEW ***/
 user_pref("browser.tabs.firefox-view", false);
